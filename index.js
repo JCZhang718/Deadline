e45064957d7265184dca012b0df1cb188bb58412
JC <jczhang718@gmail.com>
2017-11-01 16:06:02 +0000

    Add files

---
+++ index.js
@@ -0,0 +1,117 @@
+'use strict';
+
+function div() {
+	return $('<div>');
+}
+
+function showDeadlines(deadlines) {
+	// Turn due dates into moments.
+	for (var i = 0; i < deadlines.length; i++) {
+		// times are in UTC.
+		deadlines[i].due = moment.utc(deadlines[i].due, "YYYY-MM-DD HH:mm").local();
+		if (!deadlines[i].due.isValid()) {
+			console.log("Deadline " + i + " not valid");
+			return;
+		}
+	}
+	
+	var today = moment();
+	var todayOffset = -2;
+	var currentDay = moment(today).add(todayOffset, 'd');
+	// Remove deadlines from too long ago.
+	while (deadlines.length > 0 && deadlines[0].due.isBefore(currentDay, 'day')) {
+		deadlines.shift();
+	}
+	
+	var main = $('#main');
+	// Add deadlines to HTML, 1 day at a time.
+	while (deadlines.length > 0) {
+		var ele = div();
+		ele.addClass('box');
+		main.append(ele);
+		
+		// Left and right divs for the day.
+		var left = div();
+		left.addClass('left');
+		ele.append(left);
+		var leftDay = div();
+		leftDay.text(currentDay.format('dd DD MMM'));
+		if (todayOffset == 0) {
+			leftDay.css('background-color', '#6d6');
+		}
+		left.append(leftDay);
+		
+		
+		// Background color.
+		var bgColor = 'none';
+		var dayOfWeek = currentDay.day();
+		if (dayOfWeek == 0 || dayOfWeek == 6) {
+			bgColor = '#cfc';
+		}
+		
+		var red = '#fbb';
+		// If deadlines on currentDay.
+		if (deadlines.length > 0 && deadlines[0].due.isSame(currentDay, 'day')) {
+			bgColor = '#ffc';
+			// Before today.
+			if (todayOffset < 0) {
+				bgColor = red;
+			}
+			var right = div();
+			right.addClass('right');
+			ele.append(right);
+			
+			// Add each deadline on this day.
+			while (deadlines.length > 0 && deadlines[0].due.isSame(currentDay, 'day')) {
+				var deadline = deadlines.shift();
+				// When the lab ends.
+				var labEnd;
+				if (deadline.lab_hours !== undefined) {
+					labEnd = moment(deadline.due).add(deadline.lab_hours, 'h');
+				}
+				
+				var deadlineDiv = div();
+				if (deadline.due.isBefore(today)) {
+					var deadlineDivColor = red;
+					if (deadline.lab_hours !== undefined && labEnd.isAfter(today)) {
+						// Orange if lab is now.
+						deadlineDivColor = '#fc8';
+					}
+					deadlineDiv.css('background-color', deadlineDivColor);
+				}
+				right.append(deadlineDiv);
+				
+				var deadlineLeft = div();
+				deadlineLeft.addClass('deadlineLeft');
+				deadlineDiv.append(deadlineLeft);
+				if (deadline.lab_hours === undefined) {
+					deadlineLeft.text(deadline.due.format('HH:mm'));
+				} else {
+					var timeDiv = div();
+					timeDiv.text(deadline.due.format('HH:mm') + '-' + labEnd.format('HH:mm'));
+					deadlineLeft.append(timeDiv);
+				}
+				
+				var deadlineRight = div();
+				deadlineRight.addClass('deadlineRight');
+				deadlineDiv.append(deadlineRight);
+				
+				var moduleDiv = div();
+				moduleDiv.html(deadline.module);
+				deadlineRight.append(moduleDiv);
+				
+				var taskDiv = div();
+				taskDiv.html(deadline.task);
+				deadlineRight.append(taskDiv);
+			}
+		} else {
+			// Make day and date fit on 1 line.
+			//left.css('width', '100px');
+		}
+		
+		ele.css('background-color', bgColor);
+		// Next day.
+		todayOffset++;
+		currentDay.add(1, 'd');
+	}
+};
---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+	/*
+	 * Get this filename with a timestamp on the end, creating the file
+	 * if it doesn't exist. This prevents caching problems.
+	 */
+	function get_time_filename($filename) {
+		if (!file_exists($filename)) {
+			throw new Exception('File does not exist [' . $filename . ']');
+		}
+
+		$timestamp = filemtime($filename);
+		$time_filename = pathinfo($filename, PATHINFO_FILENAME) . '_' . $timestamp .
+			'.' . pathinfo($filename, PATHINFO_EXTENSION);
+
+		// Create the file if it doesn't exist.
+		if (!file_exists($time_filename)) {
+			copy($filename, $time_filename);
+		}
+
+		return $time_filename;
+	}
+
+	$indexjs_filename = get_time_filename('index.js');
+	$json = file_get_contents('deadlines.json');
+?>
+<head>
+	<title>ICS Deadlines</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono" rel="stylesheet">
+	<style>
+		* {
+			box-sizing: border-box;
+		}
+		
+		html {
+			font-family: 'Open Sans', sans-serif;
+		}
+		
+		.box {
+			padding: 5px;
+			overflow: auto;
+			border-top: solid 2px #d0e0e0;
+			max-width: 600px;
+			font-size: 0.9em;
+		}
+		
+		.left {
+			font-family: 'Share Tech Mono', monospace;	
+		}
+		.left > div {
+			display: inline-block;
+			padding: 3px; /* The font has very little padding. */
+		}
+		
+		.deadlineLeft {
+			font-family: 'Share Tech Mono', monospace;
+			float: left;
+			padding: 3px 0; /* The font has very little padding. */
+			width: 110px;
+			text-align: center;
+		}
+		
+		.deadlineRight {
+			margin-left: 110px;
+		}
+		
+		#disclaimer {
+			font-size: 0.8em;
+			margin-bottom: 20px;
+		}
+		
+		#made {
+			font-size: 0.8em;
+			margin-top: 50px;
+		}
+		#made > span {
+			display: inline-block;
+			position: relative;
+			top: -12px;
+			width: 0;
+			font-size: 0.5em;
+		}
+		.notice {
+			font-size: 0.8em;
+			margin-top: 30px;
+			font-weight: bold;
+		}
+
+		@media (min-width:768px) {
+			.left {
+				float: left;
+				width: 80px;	
+			}
+			
+			.right {
+				margin-left: 80px;
+			}
+		}
+	</style>
+</head>
+<div id="disclaimer">Disclaimer: Do not trust this information under any circumstances.</div>
+<div id="main"></div>
+<!--
+<div id="complier" class="notice">The first four exercises for Complier Design don't have officially assigned deadlines, so their deadlines here are going to be the release date of the following exercise. <br/> Except Exercise 3 and 4. These ones you actually need to submit. </div>
+-->
+<div id="made">Made with love by JC and <span>mainly</span>Dave &lt;3</div>
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js"></script>
+<script src="<?=$indexjs_filename?>"></script>
+<script>
+	showDeadlines(<?=$json?>);
+</script>
